fcb9afe1b3b78ee110a634893f8679c51fad807d
Mario Nikolov <m.nikolov@isystems.bg>
2018-12-09 19:31:08 +0200

    add home component; add details components; add routing;

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DetailsComponent } from './components/details/details.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { WeatherService } from './services/weather.service';
+import { DetailsComponent } from './components/details/details.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, HomeComponent, DetailsComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
